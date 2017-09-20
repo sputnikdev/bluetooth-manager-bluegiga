05b1dac13aa20bb3ac502c453bcf68c30f0d79ca
Vlad Kolotov <vkolotov@gmail.com>
2017-09-20 14:08:00 +1200

    Maven pom clean up Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

---
+++ .travis/install-dependencies.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn install:install-file -Dfile=lib/com.zsmartsystems.bluetooth.bluegiga-1.0.0-SNAPSHOT.jar -DgroupId=com.zsmartsystems.bluetooth.bluegiga -DartifactId=com.zsmartsystems.bluetooth.bluegiga -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
