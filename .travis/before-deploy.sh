b73d61b5fa56043e631413219705f368136efd42
Vlad Kolotov <vkolotov@gmail.com>
2017-09-21 14:36:22 +1200

    Updating gpg secrets Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]
 then
-    openssl aes-256-cbc -K $encrypted_05df9d2256fa_key -iv $encrypted_05df9d2256fa_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_12fbd8a79a40_key -iv $encrypted_12fbd8a79a40_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
